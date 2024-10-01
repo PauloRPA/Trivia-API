8c903d57e8bb20b59bd1c7c76f2916739a1da7ef
Paulo Ricardo <pauloricardopaa@gmail.com>
2024-10-01 09:01:18 -0300

    Projeto criado

---
+++ src/test/java/com/prpa/trivia/TriviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prpa.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
